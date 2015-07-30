e880c0be75379cd890e780b50a864a1a50251c79
Baer <baer.bradford@gmail.com>
2015-07-29 20:20:31 -0500

    Added barebones project for windows launcher.

---
+++ WpfLauncher/WpfLauncher/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FinalParsec.Apollo.WpfLauncher")]
+[assembly: AssemblyDescription("Digital distribution and content delivery for an indie game studio.")]
+[assembly: AssemblyCompany("Final Parsec")]
+[assembly: AssemblyProduct("WpfLauncher")]
+[assembly: AssemblyCopyright("© 2015 Final Parsec - All Rights Reserved")]
+[assembly: ComVisible(false)]
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+[assembly: AssemblyVersion("15.0.0.0")]
+[assembly: AssemblyFileVersion("15.7.29.0")]
